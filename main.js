f0d959b2c2f054e567fa18bf3fe4653d7055f0ef
victrcs01 <victrcs@ufmg.br>
2022-10-30 22:42:57 -0300

    Ajustes de acessibilidade + eficiência do código

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio' ) {
+        elemento.play();
+    }
+    else {
+        alert('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador =0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
-    //template string -> string com conteúdo variável. Usa crase para definir.
-    const idAudio = `#som_${instrumento}`;
-
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string -> string com conteúdo variável. Usa crase para definir.
+    console.log(idAudio);
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    
-    contador = contador + 1;
-    
-    //console.log(contador);
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
